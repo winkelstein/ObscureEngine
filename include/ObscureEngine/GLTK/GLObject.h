ef42e341c28041626db1500a56d0ad697a1c660f
Andrew Winkelstein <andrew.winkelstein@gmail.com>
2024-07-13 02:06:12 +0300

    feat(GLTK/GLObject): add virtual method to check is object is binded

--- include/ObscureEngine/GLTK/GLObject.h
+++ include/ObscureEngine/GLTK/GLObject.h
@@ -10,6 +10,9 @@ namespace ObscureEngine
     protected:
       GLuint instance;
 
+    protected:
+      virtual bool isBinded() = 0;
+
     public:
       virtual void bind() = 0;
       virtual void unbind() = 0;
