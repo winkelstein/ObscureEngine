064f65503e73acabfc563f8bab0ca5887cd9f0ef
Andrew Winkelstein <andrew.winkelstein@gmail.com>
2024-07-13 01:58:31 +0300

    feat(GLTK): add GLObject.h

---
+++ include/ObscureEngine/GLTK/GLObject.h
@@ -0,0 +1,17 @@
+#include "Core.h"
+
+namespace ObscureEngine
+{
+  namespace GLTK
+  {
+    class GLObject
+    {
+    protected:
+      GLuint instance;
+
+    public:
+      virtual void bind() = 0;
+      virtual void unbind() = 0;
+    };
+  }
+}
