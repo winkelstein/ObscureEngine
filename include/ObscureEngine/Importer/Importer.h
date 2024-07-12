1531840738e530f00c3dd189159720806cc6d3fb
Andrew Winkelstein <andrew.winkelstein@gmail.com>
2024-07-13 02:07:01 +0300

    feat(Importer): add virtual class Importer

---
+++ include/ObscureEngine/Importer/Importer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+#include <filesystem>
+#include <string>
+#include <utility>
+
+namespace ObscureEngine
+{
+  namespace Importer
+  {
+    template <class _Ty>
+    class Importer
+    {
+    public:
+      virtual std::pair<std::string, std::shared_ptr<_Ty>> import(std::filesystem::path path_to_config) = 0;
+    };
+  }
+}
